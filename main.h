16f385c25f9218619649d455ae043b6be1f6ac5d
Diogo-NB <diogonunes.20022@gmail.com>
2024-11-30 17:30:01 -0300

    feat: v2

--- main.h
+++ main.h
@@ -10,7 +10,7 @@
 #use FIXED_IO(D_outputs=PIN_D7,PIN_D6,PIN_D5,PIN_D4,PIN_D3,PIN_D2,PIN_D1,PIN_D0)
 #use FIXED_IO(A_outputs=PIN_A5)
 
-#use delay(crystal=8MHz)
+#use delay(crystal=4MHz)
 
 #define S1   PIN_B0
 #define S2   PIN_B1
